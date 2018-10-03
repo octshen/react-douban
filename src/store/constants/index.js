65008e54b9268d7b0668db02d391ea2fba2d14e6
octshen <196697533@qq.com>
2018-10-04 05:22:30 +0800

    Update index.js
    update constants

--- src/store/constants/index.js
+++ src/store/constants/index.js
@@ -1,8 +1,13 @@
+// user
 export const USERINFO_UPDATE = 'USERINFO_UPDATE'
 export const ADDUSER = 'ADDUSER'
+
+// activities
 export const LOADMORE = 'LOADMORE'
+export const UPDATESKIP = 'UPDATESKIP'
 export const GETSINGLEEVENT = 'GETSINGLEEVENT'
 
+// movie
 export const UPDATEHOT = 'UPDATEHOT'
 export const UPDATENEW = 'UPDATENEW'
 export const UPDATETOP = 'UPDATETOP'
