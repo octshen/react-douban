daa02a1fb9e3aeab86610ec005a0f1b72f114e77
shenmz <196697533@qq.com>
2018-10-10 10:57:04 +0800

    change axios baseUrl

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-axios.defaults.withCredentials = true
-axios.defaults.baseURL = '/api'
+// axios.defaults.withCredentials = true
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? '/api' : 'https://api.douban.com/v2'
 
 export default axios
