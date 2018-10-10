7198f09ff9284b5122c3498730ba3544a8c3429c
shenmz <196697533@qq.com>
2018-10-10 14:23:37 +0800

    add hot-load

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import Routes from './router/index'
 
@@ -8,13 +8,39 @@ import './assets/normalize.less'
 import 'antd-mobile/dist/antd-mobile.less'
 import createStore from './store/createStore'
 
+import { AppContainer } from 'react-hot-loader'
+
 // import registerServiceWorker from './registerServiceWorker';
 const store = createStore()
 window.REDUX_STORE = store
 
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  )
+}
+
+const root = document.getElementById('root')
+const render = Component => {
+  const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate
+  renderMethod(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    root
+  )
+}
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./router/index', () => { // 当我们热更新的代码出现的时候，把App重新加载
+    render(App) // 重新渲染到 document 里面
+  })
+}
+
 ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
+  <App></App>,
   document.getElementById('root')
 )
